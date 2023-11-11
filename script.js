cbfc65359e28223eea1104675793199a4053decf
Diego Melo <140443998+DiegoHenriqueMelo@users.noreply.github.com>
2023-11-11 10:42:58 -0300

    Realizei alterações  de bug no JS

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ function calcular() {
     var nome = String(txtv.value)
     var sob = String(txtv1.value)
     var val = Number(txtv2.value)
-    res.innerHTML = `Olá ${nome} ${sob}, no valor de R$${val}mil temos 7 carros à venda!`
-
+    if(nome ===  "" || sob === "" || val === 0){
+        res.innerHTML = "[ERRO] INSIRA DADOS NO CAMPO ACIMA"
+    }else{
+        res.innerHTML = `Olá ${nome} ${sob}, no valor de R$${val}mil temos 7 carros à venda!`
+    }
 }
 function limpar(){
     var res = window.document.querySelector('div#res')
@@ -30,31 +33,37 @@ function clickMenu(){
 
 function clickFav1(){
     var resv1 = document.querySelector('div.res-1')
-    let resposta1 = (prompt('Você quer que este veiculo seja Favoritado?'))
+    let resposta1 = (prompt('Você quer que este veiculo seja Favoritado? Digite 1 para Sim e 2 para Não'))
 
-if(resposta1 =="Sim" ){
+if(resposta1 == 1 ){
     resv1.innerHTML = '<img src=img/coração2.png img>'
-}else if(resposta1 =='Não'){
+}else if(resposta1 == 2){
+    resv1.innerHTML ='<img src=img/coração1.png img>'
+} else{
     resv1.innerHTML ='<img src=img/coração1.png img>'
 }
 }
 function clickFav2(){
     var resv2 = document.querySelector('div.res-2')
-    let resposta1 = (prompt('Você quer que este veiculo seja Favoritado?'))
+    let resposta1 = (prompt('Você quer que este veiculo seja Favoritado? Digite 1 para Sim e 2 para Não'))
 
-if(resposta1 =="Sim"){
+if(resposta1 == 1){
     resv2.innerHTML = '<img src=img/coração2.png img>'
-}else if(resposta1 =='Não'){
+}else if(resposta1 == 2){
     resv2.innerHTML ='<img src=img/coração1.png img>'
+}else{
+    resv1.innerHTML ='<img src=img/coração1.png img>'
 }
 }
 function clickFav3(){
     var resv3 = document.querySelector('div.res-3')
-    let resposta1 = (prompt('Você quer que este veiculo seja Favoritado?'))
+    let resposta1 = (prompt('Você quer que este veiculo seja Favoritado? Digite 1 para Sim e 2 para Não'))
 
-if(resposta1 =="Sim"){
+if(resposta1 == 1){
     resv3.innerHTML = '<img src=img/coração2.png img>'
-}else if(resposta1 =='Não'){
+}else if(resposta1 == 2){
     resv3.innerHTML ='<img src=img/coração1.png img>'
+}else{
+    resv1.innerHTML ='<img src=img/coração1.png img>'
 }
 }
